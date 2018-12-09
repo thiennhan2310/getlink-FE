303cca84abe16d41657c3933aa71b84cec8cdc66
nhantt <nhan.tran@navigosgroup.com>
2018-12-09 12:51:45 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DetailPage from './page/Detail';
+import LoginPage from './page/Login';
+import HomePage from './page/Home';
+
+import {
+  BrowserRouter,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <ul>
+              <li><Link to="/">Home</Link></li>
+            </ul>
+          </header>
+          <Switch>
+            <Route exact path="/" render={HomePage} />
+            <Route exact path="/login" render={LoginPage} />
+            <Route path="/article/:id" component={DetailPage} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
