77eed214bc52b9ed077dbb7c6e70f14f3e744d2c
nhantt <nhan.tran@navigosgroup.com>
2018-12-10 14:46:09 +0700

    Updates

---
+++ precache-manifest.790be608cae69475fc80e25d7bb68cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gelink-FE/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9088d5155bbecc4c232c",
+    "url": "/gelink-FE/static/js/runtime~main.9088d515.js"
+  },
+  {
+    "revision": "32219d524647d9278e9e",
+    "url": "/gelink-FE/static/js/main.32219d52.chunk.js"
+  },
+  {
+    "revision": "79ea2e5848ebf1b9961c",
+    "url": "/gelink-FE/static/js/1.79ea2e58.chunk.js"
+  },
+  {
+    "revision": "32219d524647d9278e9e",
+    "url": "/gelink-FE/static/css/main.a156bafd.chunk.css"
+  },
+  {
+    "revision": "fb3e275f755cc68172bc9bce6169bb49",
+    "url": "/gelink-FE/index.html"
+  }
+];
